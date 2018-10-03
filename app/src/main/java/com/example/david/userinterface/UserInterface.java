6029c2303a8b1fde87b6474ff545ec17ee26591a
David <davidangulocorcuera@gmail.com>
2018-10-03 21:06:10 +0200

    show description do it!

--- app/src/main/java/com/example/david/userinterface/UserInterface.java
+++ app/src/main/java/com/example/david/userinterface/UserInterface.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +27,9 @@ public class UserInterface extends AppCompatActivity {
     private TextInputLayout til_postalcode;
     private TextInputLayout til_city;
 
+    private Switch switch_hide;
+    private Boolean bl_switchState;
+    private TextView textView_description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,25 +40,37 @@ public class UserInterface extends AppCompatActivity {
         String str_firstSurname = et_firstSurname.getText().toString();
         String str_secondSurname = et_secondSurname.getText().toString();
         String str_postalCode = et_postalCode.getText().toString();
-        if(hidePostalCodeAndCity(str_name,str_firstSurname)){
+        //til_city =(TextInputLayout) til_city.findViewById(R.id.textInputLayout_city);
+        /*if(showPostalCodeAndCity(str_name,str_firstSurname)){
             til_city.setVisibility(View.VISIBLE);
+        }*/
+        switch_hide =  (Switch) findViewById(R.id.switch_hide);
+        textView_description  = findViewById(R.id.textView_description);
+        bl_switchState = switch_hide.isChecked();
+    }
+    public void showDescription(View view) {
+        if(!(switch_hide.isChecked())){
+            textView_description.setVisibility(View.INVISIBLE);
+        }
+        else{
+            textView_description.setVisibility(View.VISIBLE);
         }
     }
-
     public void showPopUp(View view) {
         Toast toast = new Toast(this);
         toast.setGravity(5, 6, 6);
         Toast.makeText(this, "name: \n" + et_name.getText().toString() + " first surname: \n" +
                 et_firstSurname.getText().toString() + "second surname \n" + et_secondSurname.getText().toString() + " postal code: \n" + et_postalCode.getText().toString()
                 + " ciudad: \n" + et_city.getText().toString(), Toast.LENGTH_SHORT).show();
+
     }
 
 
-    public boolean hidePostalCodeAndCity(String str_name, String str_surname) {
-        if (!(str_name.equals("") && str_surname.equals(""))) {
-            return true;
-        } else {
+    public boolean showPostalCodeAndCity(String str_name, String str_surname) {
+        if (str_name.equals("") && str_surname.equals("")) {
             return false;
+        } else {
+            return true;
         }
     }
 }
